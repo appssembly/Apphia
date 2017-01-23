d2e7445ae99f27eef201b37d37b79b3a9d6433af
appssembly <saleem-azad@outlook.com>
2017-01-23 14:28:22 +1100

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -31,8 +31,8 @@ class BetterSmoochApiBot extends SmoochApiBot {
     }
 }
 
-const name = 'SmoochBot';
-const avatarUrl = 'https://s.gravatar.com/avatar/f91b04087e0125153623a3778e819c0a?s=80';
+const name = 'Charlie';
+const avatarUrl = 'https://raw.githubusercontent.com/adidibmx/EstherBot/master/img/charlie.jpg';
 const store = new SmoochApiStore({
     jwt
 });
